3dda6a63bc23788bee626a76b22e5c8e96c1e0d0
Olaide Ojewale <ojewaleolaide@gmail.com>
2015-07-07 18:26:33 +0100

    Corrected problem2 file

--- problem2.py
+++ problem2.py
@@ -1,17 +1,16 @@
 import time
 import webbrowser
-timez = [["google.com",10.0],["andela.co",20.0],["google.com",30.0]]
+timez = [[url1,time1],[url1,time1],[url1,time1]]
 
 def open_url(tim):
 	for addy in tim:
 		lengthy = len(addy)
 		last_item = addy[lengthy-1]
-		for single_list in addy:
-			url = single_list[0]
-			delay_time = single_list[1]
-			if single_list != last_item:
-				webbrowser.open(url)
-				time.sleep(delay_time)
-			else:
-				webbrowser.open(url)
+		url = addy[0]
+		delay_time = addy[1]
+		if addy != last_item:
+			webbrowser.open(url)
+			time.sleep(delay_time)
+		else:
+			webbrowser.open(url)
 open_url(timez)
