3a966ccffdd042ae61db813cae9a85fa0c86e520
Olaide Ojewale <ojewaleolaide@gmail.com>
2015-07-07 18:25:37 +0100

    Corrected problem1

--- problem1.py
+++ problem1.py
@@ -2,9 +2,8 @@ import time
 import webbrowser
 
 url = 'www.google.com'
-webbrowser.open(url)
-time.sleep(5)
-webbrowser.open(url)
-time.sleep(5)
-webbrowser.open(url)
-time.sleep(5)
+kount = 0
+while kount < 3:
+	webbrowser.open(url)
+	time.sleep(5)
+	kount +=1
